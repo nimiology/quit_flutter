b63c5a4dbbe4f51479cab1eeb585f53de2b7d7ec
Nima <nimiologyy@gmail.com>
2022-08-25 18:23:21 +0430

    doing widget

--- lib/widgets/doing_widget.dart
+++ lib/widgets/doing_widget.dart
@@ -13,57 +13,49 @@ class DoingWidget extends StatefulWidget {
 class _DoingWidgetState extends State<DoingWidget> {
   @override
   Widget build(BuildContext context) {
+    Color errorColor = Theme.of(context).errorColor;
     Doing doing = widget.doing;
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-      padding: EdgeInsets.symmetric(horizontal: 10),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(15),
-        child: Dismissible(
-          key: ObjectKey(doing.id),
-          background: Container(
-            alignment: Alignment.centerRight,
-            padding: const EdgeInsets.only(right: 20),
-            decoration: BoxDecoration(
-              // borderRadius: BorderRadius.circular(25),
-              color: Theme.of(context).errorColor,
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+
+          gradient: LinearGradient(
+              colors: [errorColor, errorColor.withOpacity(0.7)]),
+          boxShadow: [
+            BoxShadow(
+              color: errorColor.withOpacity(0.5),
+              spreadRadius: 5,
+              blurRadius: 10,
+              offset: Offset(0, 3), // changes position of shadow
             ),
-            child: const Icon(
-              Icons.delete,
-              size: 30,
-            ),
-          ),
-          onDismissed: (direction) {
-            setState(() {});
-          },
-          child: Container(
-            color: Theme.of(context).accentColor,
-            child: ListTile(
-              title: Text(
-                '${doing.date.hour}:${doing.date.minute}',
-                style: TextStyle(color: Colors.white),
-              ),
-              subtitle: Text(
-                '${doing.why}',
-                style: TextStyle(color: Colors.white70),
-              ),
-              trailing: Container(
-                width: 50,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(Icons.edit),
-                    Icon(
-                      Icons.delete,
-                      color: Colors.red,
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+          ],
         ),
-      ),
-    );
+        child: ListTile(
+          title: Text(
+            '${doing.date.hour}:${doing.date.minute}',
+            style: TextStyle(color: Colors.white),
+          ),
+          subtitle: Text(
+            '${doing.why}',
+            style: TextStyle(color: Colors.white70),
+          ),
+          trailing: Container(
+            width: 65,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.edit),
+                SizedBox(
+                  width: 15,
+                ),
+                Icon(
+                  Icons.delete,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
